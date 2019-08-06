5aba3e79c5e73053665efe83ccf10e32f75d0cd3
GreensladeNZ <greensladenz@gmail.com>
2019-08-06 18:51:37 +1200

    AU915 set to default

--- TinyLoRa.h
+++ TinyLoRa.h
@@ -73,9 +73,9 @@ typedef enum rfm_datarates
 } rfm_datarates_t;
 
 /** Region configuration*/
-#define US902 ///< Used in USA, Canada and South America
+//#define US902 ///< Used in USA, Canada and South America
 //#define EU863 ///< Used in Europe
-//#define AU915 ///< Used in Australia
+#define AU915 ///< Used in Australia, New Zealand
 //#define AS920 ///< Used in Asia
 
 #define RFM9x_VER   0x12 ///<Expected RFM9x RegVersion
